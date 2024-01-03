826ae39155bc52f93f8f000c2043aba0c8c4f797
abdullahinalcik <130457799+abdullahinalcik@users.noreply.github.com>
2024-01-03 17:47:20 +0300

    add:class notes

--- 05-V-Bind/app.js
+++ 05-V-Bind/app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
 
     template:`
+    <div :style="style" > Deneme Yazısı </div>
    
     `,
     data() {
         return {
-           
+           style:"color:red"
    
         }
     },
